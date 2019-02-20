894eb90b222003541209afcff8ec4ffc5c6dedb9
Marta Rutkowska <marta.rutkowska@protonmail.com>
2019-02-20 14:41:02 +0100

    room interior modified using asset store assets, interaction with window added, post- processiong added

--- 3D Game Practise/Assets/Scripts/PlayerController.cs
+++ 3D Game Practise/Assets/Scripts/PlayerController.cs
@@ -1,15 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
     [SerializeField] private float movementSpeed = 0.1f;
     [SerializeField] private float rotationSpeed = 0.2f;
+    [SerializeField] private float jumpSpeed = 1f;
     [SerializeField] private float maxRotationX = 100f;
     [SerializeField] private float minRotationX = -100f;
     [SerializeField] private Transform mainCamera;
-
+    [SerializeField] private float windowInteractionDistance = 4f;
+    [SerializeField] private Image interactionSign;
 
     private Rigidbody playerRigidbody;
     private Vector3 movementVector = Vector3.zero;
@@ -17,7 +20,11 @@ public class PlayerController : MonoBehaviour {
     private float yRotation;
     private float xRotation;
 
+    private bool isGrounded = false;
     private float playerYRotation;
+
+    private RaycastHit raycastHit;
+    private Vector3 forwardVector;
 
 	void Start () {
         playerRigidbody = GetComponent<Rigidbody>();
@@ -28,12 +35,51 @@ public class PlayerController : MonoBehaviour {
     {
         PlayerMovement();
         PlayerRotation();
-
+        PlayerJump();
+        RaycastInteraction();
     }
 
     void LateUpdate () {
         CameraRotation();
 	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+        
+    }
+
+    private void RaycastInteraction()
+    {
+        forwardVector = mainCamera.TransformDirection(Vector3.forward);
+
+        if (Physics.Raycast(transform.position, forwardVector, out raycastHit, windowInteractionDistance))
+        {
+            if(raycastHit.collider.gameObject.tag == "Window")
+            {
+                interactionSign.enabled = true;
+                OpenTheWindow(raycastHit.collider.gameObject);
+            } else
+            {
+                interactionSign.enabled = false;
+            }
+        }
+        else
+        {
+            interactionSign.enabled = false;
+        }
+    }
+
+    private void OpenTheWindow(GameObject currentWindow)
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            currentWindow.transform.GetComponentInParent<WindowScript>().WindowInteraction();
+        }
+    }
 
     private void PlayerRotation()
     {
@@ -58,7 +104,14 @@ public class PlayerController : MonoBehaviour {
         
         movementVector = new Vector3(Input.GetAxis("Horizontal")*movementSpeed, playerRigidbody.velocity.y, Input.GetAxis("Vertical")*movementSpeed);
         movementVector = transform.TransformDirection(movementVector);
-        //Debug.Log(movementVector);
         playerRigidbody.velocity = movementVector;
+    }
+
+    private void PlayerJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            playerRigidbody.velocity = new Vector3(playerRigidbody.velocity.x, jumpSpeed, playerRigidbody.velocity.z); 
+        }
     }
 }
---
+++ 3D Game Practise/Assets/Scripts/WindowScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindowScript : MonoBehaviour {
+
+    [SerializeField] private float openSpeed = 30;
+
+    private bool isOpened = false;
+
+    private float baseRotationY;
+    private float desiredRotationY;
+    private float currentRotationY;
+
+    private bool rotating = false;
+
+    private void Start()
+    {
+        baseRotationY = transform.rotation.y;
+        currentRotationY = baseRotationY;
+    }
+
+    public void WindowInteraction()
+    {
+        currentRotationY = transform.rotation.y;
+        isOpened = !isOpened;
+
+        if (isOpened)
+            desiredRotationY = baseRotationY + 90f;
+        else
+            desiredRotationY = baseRotationY;
+
+        StartCoroutine(Rotate());
+    }
+
+    private IEnumerator Rotate()
+    {
+        if (!rotating)
+        {
+            rotating = true;
+            Quaternion rot = Quaternion.Euler(transform.eulerAngles.x, desiredRotationY, transform.eulerAngles.z);
+            while (Mathf.Abs(transform.eulerAngles.y - desiredRotationY) > 1.0f)
+            {
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, rot, openSpeed * Time.deltaTime);
+                yield return new WaitForEndOfFrame();
+            }
+
+            rotating = false;
+        }
+    }
+}
